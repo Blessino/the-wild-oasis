615ecf7cd326ddca6330905bdf68f5c35df24b75
Ashish <grunch.me1@gmail.com>
2024-10-18 11:03:24 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import GlobalStyles from "./styles/GlobalStyles";
+import styled from "styled-components";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onClick={() => alert("Check out")}>Check out</Button>
+
+        <Input type="number" placeholder="Number of guest" />
+        <Input type="number" placeholder="Number of guest" />
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
