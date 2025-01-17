306fa8f67e5b53879a4bf384b08048540e7f15ed
Ashish <grunch.me1@gmail.com>
2025-01-17 13:00:13 +0545

    cabin table header is created

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,3 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCabins } from "../../services/apiCabin";
+import { Spinner } from "../../ui/Spinner";
+import CabinRow from "./CabinRow"
+
 import styled from "styled-components";
 
 const Table = styled.div`
@@ -23,3 +28,32 @@ const TableHeader = styled.header`
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
 `;
+
+function CabinTable() {
+  const {
+    isLoading,
+    data: cabins,
+    error,
+  } = useQuery({
+    queryKey: ["cabin"],
+    queryFn: getCabins,
+  });
+
+  if (isLoading) return <Spinner />;
+
+  return (
+    <Table role="table">
+      <TableHeader role="row">
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </TableHeader>
+      { cabins.map(cabin=><CabinRow cabin={cabin} key={cabin.id}/>)}
+    </Table>
+  );
+}
+
+export default CabinTable;
