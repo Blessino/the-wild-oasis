9f863ecc68712b5395a48b5ef28df38c637e3e7e
Ashish <grunch.me1@gmail.com>
2025-01-23 15:47:57 +0545

    filter and sort is working in cabin table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,30 +1,39 @@
-import Spinner from "../../ui/Spinner";
-import CabinRow from "./CabinRow";
+import { useSearchParams } from "react-router-dom";
 
-import styled from "styled-components";
+import CabinRow from "./CabinRow";
 import { useCabin } from "./useCabin";
+
+import Spinner from "../../ui/Spinner";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
-
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`;
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabin();
+  const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+  if (!cabins.length) return <Empty resourceName="cabins" />;
+
+  //Filter
+  const filterValue = searchParams.get("discount") || "all";
+
+  let filteredCabins;
+  if (filterValue === "all") filteredCabins = cabins;
+  if (filterValue === "no-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  if (filterValue === "with-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  //Sort
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  console.log(field, direction)
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
+  // console.log(modifier, sortedCabins)
 
   return (
     <Menus>
@@ -39,7 +48,9 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={cabins}
+          // data={cabins}
+          // data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,6 +1,17 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
+export async function apiBookings() {
+  const { data, error } = await supabase.from("booking").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking copuld not be loaded");
+  }
+
+  return data;
+}
+
 export async function getBooking(id) {
   const { data, error } = await supabase
     .from("bookings")
