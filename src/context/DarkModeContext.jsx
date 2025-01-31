79dfa86f4f93df19033bac4775ca4ed799f993bd
Ashish <grunch.me1@gmail.com>
2025-01-31 16:28:59 +0545

    dark mode apply as of the device setting

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -4,7 +4,10 @@ import { useLocalStorageState } from "../hooks/useLocalStorageState";
 const DarkModeContext = createContext();
 
 function DarkModeProvider({ children }) {
-  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(
+    window.matchMedia("(prefers-color-scheme: dark)").matches,
+    "isDarkMode"
+  );
 
   useEffect(
     function () {
@@ -12,8 +15,8 @@ function DarkModeProvider({ children }) {
         document.documentElement.classList.add("dark-mode");
         document.documentElement.classList.remove("light-mode");
       } else {
-           document.documentElement.classList.remove("dark-mode");
-           document.documentElement.classList.add("light-mode");
+        document.documentElement.classList.remove("dark-mode");
+        document.documentElement.classList.add("light-mode");
       }
     },
     [isDarkMode]
